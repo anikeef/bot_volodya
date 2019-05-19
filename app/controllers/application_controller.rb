8ed4230f22338806bbac10852a49f82bcdb2bc5f
Gennadiy <genaydzhan70@gmail.com>
2019-05-19 23:32:01 +0600

    Add status: :ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,10 +11,9 @@ class ApplicationController < ActionController::API
       user_id = params["object"]["from_id"]
       message = params["object"]["text"]
       modified_message = CGI.escape("#{message}? Хороший вопрос")
-      srand
       random_id = Random.rand(10000000000)
       RestClient.get "https://api.vk.com/method/messages.send?user_id=#{user_id}&random_id=#{random_id}&message=#{modified_message}&access_token=#{ENV["VK_KEY"]}&v=5.95"
-      render text: "ok"
+      render status: :ok, text: "ok"
     end
   end
 end
