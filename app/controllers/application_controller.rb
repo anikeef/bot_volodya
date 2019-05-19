950bc726261c7818d0ae7c8655a002d40f96cb1f
Gennadiy <genaydzhan70@gmail.com>
2019-05-19 21:35:48 +0600

    Add random_id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,8 @@ class ApplicationController < ActionController::API
       user_id = params["object"]["from_id"]
       message = params["object"]["text"]
       modified_message = "#{message}? Хороший вопрос"
-      RestClient.post "https://api.vk.com/method/messages.send?user_id=#{user_id}&message=#{modified_message}&access_token=#{ENV["VK_KEY"]}&v=5.95"
+      random_id = rand(10000000000)
+      RestClient.post "https://api.vk.com/method/messages.send?user_id=#{user_id}&random_id=#{random_id}&message=#{modified_message}&access_token=#{ENV["VK_KEY"]}&v=5.95"
       render text: "ok"
     end
   end
