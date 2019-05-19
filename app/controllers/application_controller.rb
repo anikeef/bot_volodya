e7e15cabfea06f554aaa2e098ebe83813cf78ed1
Gennadiy <genaydzhan70@gmail.com>
2019-05-19 23:37:54 +0600

    Try to use smaller random number

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::API
       user_id = params["object"]["from_id"]
       message = params["object"]["text"]
       modified_message = CGI.escape("#{message}? Хороший вопрос")
-      random_id = Random.rand(10000000000)
+      random_id = Random.rand(10000000)
       RestClient.get "https://api.vk.com/method/messages.send?user_id=#{user_id}&random_id=#{random_id}&message=#{modified_message}&access_token=#{ENV["VK_KEY"]}&v=5.95"
       render status: :ok, text: "ok"
     end
