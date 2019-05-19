fd9acfba1eca0ddbc663c2a2a34809103859f48a
Gennadiy <genaydzhan70@gmail.com>
2019-05-19 21:28:14 +0600

    Initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,16 @@
+require "rest-client"
+
+class ApplicationController < ActionController::API
+  def request
+    case params["type"]
+    when "confirmation"
+      render text: "#{ENV["VK_CONFIRM"]}"
+    when "message_new"
+      user_id = params["object"]["from_id"]
+      message = params["object"]["text"]
+      modified_message = "#{message}? Хороший вопрос"
+      RestClient.post "https://api.vk.com/method/messages.send?user_id=#{user_id}&message=#{modified_message}&access_token=#{ENV["VK_KEY"]}&v=5.95"
+      render text: "ok"
+    end
+  end
+end
---
+++ vendor/bundle/gems/ffi-1.11.0/ext/ffi_c/libffi-x86_64-darwin17/include/ffitarget.h
@@ -0,0 +1,1 @@
+/Users/a1/ex/bot/vendor/bundle/gems/ffi-1.11.0/ext/ffi_c/libffi/src/x86/ffitarget.h
