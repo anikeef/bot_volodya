d9556520e19c4ee1980b091d28d74972d2e9f4ae
Gennadiy <genaydzhan70@gmail.com>
2019-05-20 17:19:17 +0600

    Add more messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,12 @@ class ApplicationController < ActionController::API
       render plain: "#{ENV["VK_CONFIRM"]}"
       return
     when "message_new"
-      answer { |message| "#{message}? Хорошо сказано"}
+      answer do |message|
+        ["#{message}? Хороший вопрос",
+         "Что такое \"#{message}\"?",
+         "\"#{message}\"... Хорошо сказано",
+         "Насчет \"#{message}\"... Это долгая история"].sample
+      end
       render status: :ok, plain: "ok"
     end
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
     random_id = Random.rand(10000000)
     thread = Thread.new do
       RestClient.get "https://api.vk.com/method/messages.markAsRead?start_message_id=#{message_id}&access_token=#{ENV["VK_KEY"]}&v=5.95"
-      RestClient.get "https://api.vk.com/method/messages.send?user_id=#{user_id}&random_id=#{random_id}&message=#{modified_message}&reply_message=#{message_id}&access_token=#{ENV["VK_KEY"]}&v=5.95"
+      RestClient.get "https://api.vk.com/method/messages.send?user_id=#{user_id}&random_id=#{random_id}&message=#{modified_message}&access_token=#{ENV["VK_KEY"]}&v=5.95"
     end
     thread.join
   end
