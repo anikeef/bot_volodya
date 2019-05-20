c1cdc6793c68adb4f6e7ab81a857424b94959e28
Gennadiy <genaydzhan70@gmail.com>
2019-05-20 14:26:28 +0600

    Add access token and version to markAsRead

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,8 @@ class ApplicationController < ActionController::API
       message = params["object"]["text"]
       modified_message = CGI.escape("#{message}? Хороший вопрос")
       random_id = Random.rand(10000000)
+      RestClient.get "https://api.vk.com/method/messages.markAsRead?start_message_id=#{message_id}&access_token=#{ENV["VK_KEY"]}&v=5.95"
       RestClient.get "https://api.vk.com/method/messages.send?user_id=#{user_id}&random_id=#{random_id}&message=#{modified_message}&access_token=#{ENV["VK_KEY"]}&v=5.95"
-      RestClient.get "https://api.vk.com/method/messages.markAsRead?start_message_id=#{message_id}"
       render status: :ok, text: "ok"
     end
   end
