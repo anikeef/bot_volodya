252eb7b0d29c1086061bfa08a5651e08eb07ea0e
Gennadiy <genaydzhan70@gmail.com>
2019-05-20 16:51:10 +0600

    Create #answer helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,16 +10,7 @@ class ApplicationController < ActionController::API
       render plain: "#{ENV["VK_CONFIRM"]}"
       return
     when "message_new"
-      user_id = params["object"]["from_id"]
-      message_id = params["object"]["id"]
-      message = params["object"]["text"]
-      modified_message = CGI.escape("#{message}? Хороший вопрос")
-      random_id = Random.rand(10000000)
-      thread = Thread.new do
-        RestClient.get "https://api.vk.com/method/messages.markAsRead?start_message_id=#{message_id}&access_token=#{ENV["VK_KEY"]}&v=5.95"
-        RestClient.get "https://api.vk.com/method/messages.send?user_id=#{user_id}&random_id=#{random_id}&message=#{modified_message}&access_token=#{ENV["VK_KEY"]}&v=5.95"
-      end
-      thread.join
+      VKBot.answer { |message| "#{message}? Хорошо сказано"}
       render status: :ok, plain: "ok"
     end
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def answer #Accepts a block
+    user_id = params["object"]["from_id"]
+    message_id = params["object"]["id"]
+    message = params["object"]["text"]
+    modified_message = CGI.escape(yield(message))
+    random_id = Random.rand(10000000)
+    thread = Thread.new do
+      RestClient.get "https://api.vk.com/method/messages.markAsRead?start_message_id=#{message_id}&access_token=#{ENV["VK_KEY"]}&v=5.95"
+      RestClient.get "https://api.vk.com/method/messages.send?user_id=#{user_id}&random_id=#{random_id}&message=#{modified_message}&access_token=#{ENV["VK_KEY"]}&v=5.95"
+    end
+    thread.join
+  end
 end
